9b5b2444696719fa2fd16925316c83218958565c
Diego Sano <diego.sano@nave.rs>
2022-03-07 13:39:12 -0300

    _app configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { AuthProvider } from '../contexts/AuthContext';
 
-export default MyApp
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+    </AuthProvider>
+  );
+};
+
+export default MyApp;
